887db57e9675c22133eb664ed5496c7e347e6ac4
Putrahendika <100105930+Putrahendika@users.noreply.github.com>
2023-04-01 15:21:09 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ const {
 
 //sebagian panel gk support opena ai jadi mungkin apikey lu bakalan di blokir
 //klo misalkan pada saat memakai fitur ai, terus dapat email dari open si, itu berarti website tempat lu install bot gk support ai
-global.OpenAIKey = "YOUR_KEY" //login to https://platform.openai.com/account/api-keys to get apikey
+global.OpenAIKey = "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" //login to https://platform.openai.com/account/api-keys to get apikey
 
 // Website Api (jgn di ganti tomlol)
 global.APIs = {
@@ -21,7 +21,7 @@ global.APIKeys = {
    'https://api.zeeoneofc.my.id': 'V12', // 👉 login https://api.zeeoneofc.my.id to get apikey
 }
 global.sewabotnya = `
-edit sewabot di file settings.js line 24
+permanen 5k
 `
 // setting 
 global.firtsChat = true //send firts message to new user
@@ -46,18 +46,18 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "Putra-Mdོ" //namabot kalian
+global.ownername = "Putra" //nama kalian
+global.myweb = "https://google.com" //bebas asal jan hapus
+global.youtube = "https://youtube.com/" //bebas asal jan hapus
+global.myweb2 = "https://google.com" //bebas
+global.email = "admin@planetcodex.my.id" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62821931157232"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+"+ownernomer //nmr wa kalian
+global.premium = [''] //premium user
+global.owner = ["6285173241128", ""] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6285173241128" // nomor wa kalian
+global.ownernomerr = "+6285173241128" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/edca4b513ddf29fd52c0e.jpg" // maks size 30kb, agar welcome image nya tdk delay
